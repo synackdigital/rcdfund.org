b37c45c82c421e59899a505ae283688f0db7ff49
frebro <frebro@gmail.com>
2013-09-10 12:20:20 +0200

    connor's run section

--- app/themes/rcdfund/landing.php
+++ app/themes/rcdfund/landing.php
@@ -4,7 +4,7 @@ Template Name: Landing Page
 */
 ?>
 
-<section class="banner">
+<section class="banner text-center">
   <div class="container">
     <div class="row">
       <div class="col-md-4">
@@ -12,6 +12,19 @@ Template Name: Landing Page
       </div>
       <div class="col-md-8">
         <p class="tagline"><small class="block">Supporting brain matters in areas of</small> Research, Care and Development. <small class="block">In memory of Robert Connor Dawes</small></p>
+        <p><a href="#" class="btn btn-primary btn-lg" title="Make a donation"><span class="glyphicon glyphicon-hand-right"></span>&emsp;Make a donation</a></p>
+      </div>
+    </div>
+  </div>
+</section>
+
+<section class="connors-run text-center">
+  <div class="container">
+    <div class="row">
+      <div class="col-md-8 col-md-offset-2">
+        <h1 class="section-title"><span>Connor's Run</span></h1>
+        <p class="lead">Our first event was a smashing success with 1,500 big hearted and big brained participants. Together we raised $75,000 for Research, Care &amp; Development projects. Connor would be proud. Thank you.</p>
+        <p>Event photos are up on our <a href="#">Facebook page</a> <small>(p.s you don’t need an account to view them)</small></p>
       </div>
     </div>
   </div>
--- app/themes/rcdfund/lib/scripts.php
+++ app/themes/rcdfund/lib/scripts.php
@@ -11,7 +11,7 @@
  * 3. /theme/assets/js/main.min.js (in footer)
  */
 function roots_scripts() {
-  wp_enqueue_style('roots_main', get_template_directory_uri() . '/assets/css/main.min.css', false, '5cc5b22a95b98251f5073fcf64697b5f');
+  wp_enqueue_style('roots_main', get_template_directory_uri() . '/assets/css/main.min.css', false, '89be836ed718633747e8ebb07548258d');
 
   // jQuery is loaded using the same method from HTML5 Boilerplate:
   // Grab Google CDN's latest jQuery with a protocol relative URL; fallback to local if offline
