2652b96e93d638fa3c89cdfa223eac5481ff0d99
frebro <frebro@gmail.com>
2013-09-10 13:17:37 +0200

    about section

--- app/themes/rcdfund/landing.php
+++ app/themes/rcdfund/landing.php
@@ -8,7 +8,7 @@ Template Name: Landing Page
   <div class="container">
     <div class="row">
       <div class="col-md-4">
-        <img class="logo" src="/assets/img/rcdfund-blue-sbm.png" alt="RCD Fund – Supporting Brain Matters">
+        <img class="logo img-responsive" src="/assets/img/rcdfund-blue-sbm.png" alt="RCD Fund – Supporting Brain Matters">
       </div>
       <div class="col-md-8">
         <p class="tagline"><small class="block">Supporting brain matters in areas of</small> Research, Care and Development. <small class="block">In memory of Robert Connor Dawes</small></p>
@@ -23,8 +23,44 @@ Template Name: Landing Page
     <div class="row">
       <div class="col-md-8 col-md-offset-2">
         <h1 class="section-title"><span>Connor's Run</span></h1>
-        <p class="lead">Our first event was a smashing success with 1,500 big hearted and big brained participants. Together we raised $75,000 for Research, Care &amp; Development projects. Connor would be proud. Thank you.</p>
+        <p class="lead">Our first event was a smashing success with 1,500 big hearted and big brained participants. Together we raised $75,000 for Research, Care &amp; Development projects. Connor would be proud.</p>
+        <p class="lead"><strong>Thank you.</strong></p>
         <p>Event photos are up on our <a href="#">Facebook page</a> <small>(p.s you don’t need an account to view them)</small></p>
+      </div>
+    </div>
+  </div>
+</section>
+
+<section class="about">
+  <div class="container">
+    <div class="row">
+      <div class="col-md-7">
+        <h1 class="section-title">The RCD Fund</h1>
+        <p>Inspired by a big heart and brain, the RCD Fund was created in memory of Robert Connor Dawes in June 2013.</p>
+        <p>For 16 months, Connor's brilliant mind, strong body and gentle soul faced off against an ependymoma. Like many others, his brains and brawn gave it all to fight against the poor odds. But on April 20, 2013, Connor's own battle ended.</p>
+        <p>As a tribute to him and other brain tumour fighters, we created the RCD Fund: supporting brain matters in the areas of research, care &amp; development.</p>
+        <h2>Our Mission</h2>
+        <p>Brain tumours are the most fatal of all childhood cancers. Current clinical treatments only help 50% of children, and leave 90% with lifelong physical and mental impairments. We're all about changing the odds. To make brain tumours go the way of the dinosaurs. To support the science and in the meatime the patients. Our fundraising goals are captured by Connor’s initials RCD.</p>
+      </div>
+      <div class="col-md-4 col-md-offset-1 text-center">
+        <figure>
+          <img class="img-circle img-responsive" src="/assets/img/robert-connor-dawes.jpg" alt="Photo of Robert">
+          <figcaption>Robert Connor Dawes</figcaption>
+        </figure>
+      </div>
+    </div>
+    <div class="areas row text-center">
+      <div class="col-md-4">
+        <h3>Reseach</h3>
+        <p>Contributing funds to enable researchers to further understand and more completely treat brain tumours, including earlier detection, surgery and post surgery treatments.</p>
+      </div>
+      <div class="col-md-4">
+        <h3>Care</h3>
+        <p>Supporting families with rehabilitation therapies and home assistance. This is commonly not covered by insurance, yet emotionally invaluable to both the patient and the family.</p>
+      </div>
+      <div class="col-md-4">
+        <h3>Development</h3>
+        <p>We work to help with the educational needs of other young people going through brain tumour treatment including tutoring and educational support.</p>
       </div>
     </div>
   </div>
