487036198600b6decff667579310f179e167c2b2
frebro <frebro@gmail.com>
2013-09-13 23:29:00 +0200

    areas design tweaks

--- app/themes/rcdfund/lib/scripts.php
+++ app/themes/rcdfund/lib/scripts.php
@@ -11,7 +11,7 @@
  * 3. /theme/assets/js/main.min.js (in footer)
  */
 function roots_scripts() {
-  wp_enqueue_style('roots_main', get_template_directory_uri() . '/assets/css/main.min.css', false, '23fa8e5fa89fca41c8e8324610c6be30');
+  wp_enqueue_style('roots_main', get_template_directory_uri() . '/assets/css/main.min.css', false, '07b4a7ab1c2fec5d73cabc45dc9bf9a8');
 
   // jQuery is loaded using the same method from HTML5 Boilerplate:
   // Grab Google CDN's latest jQuery with a protocol relative URL; fallback to local if offline
