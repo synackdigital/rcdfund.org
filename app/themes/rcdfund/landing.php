e26830b2e8d79e92180ae6122d1639340a9ad43e
frebro <frebro@gmail.com>
2013-09-13 11:27:23 +0200

    some color tweaks and a nicer pattern

--- app/themes/rcdfund/landing.php
+++ app/themes/rcdfund/landing.php
@@ -14,7 +14,7 @@ $img_path = get_template_directory_uri() . '/assets/img';
       </div>
       <div class="col-md-8">
         <p class="tagline"><small class="block">Supporting brain matters in areas of</small> Research, Care and Development. <small class="block">In memory of Robert Connor Dawes</small></p>
-        <p><a href="https://connorsrun.gofundraise.com.au/payments/donate/beneficiary/1023" class="btn btn-primary btn-lg" title="Make a donation"><span class="glyphicon glyphicon-hand-right"></span>&emsp;Make a donation</a></p>
+        <p><a href="https://connorsrun.gofundraise.com.au/payments/donate/beneficiary/1023" class="btn btn-info btn-lg" title="Make a donation"><span class="glyphicon glyphicon-hand-right"></span>&emsp;Make a donation</a></p>
       </div>
     </div>
   </div>
