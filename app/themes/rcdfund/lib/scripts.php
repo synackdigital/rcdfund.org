f51876bf13026501e123f012a57ce86641938024
frebro <frebro@gmail.com>
2013-09-14 10:25:46 +0200

    ref #13: notebook pattern in header and footer

--- app/themes/rcdfund/lib/scripts.php
+++ app/themes/rcdfund/lib/scripts.php
@@ -11,7 +11,7 @@
  * 3. /theme/assets/js/main.min.js (in footer)
  */
 function roots_scripts() {
-  wp_enqueue_style('roots_main', get_template_directory_uri() . '/assets/css/main.min.css', false, '0d536a726617fb60947f638746e7a91e');
+  wp_enqueue_style('roots_main', get_template_directory_uri() . '/assets/css/main.min.css', false, 'b0ea6ade495f54b7346e6fad13d4dd83');
 
   // jQuery is loaded using the same method from HTML5 Boilerplate:
   // Grab Google CDN's latest jQuery with a protocol relative URL; fallback to local if offline
