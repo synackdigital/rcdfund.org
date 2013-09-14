8e79b9f242cdf4985dbbf14af3339c57893a448a
frebro <frebro@gmail.com>
2013-09-14 14:37:51 +0200

    ref #14: connor's run section sizing

--- app/themes/rcdfund/lib/scripts.php
+++ app/themes/rcdfund/lib/scripts.php
@@ -11,7 +11,7 @@
  * 3. /theme/assets/js/main.min.js (in footer)
  */
 function roots_scripts() {
-  wp_enqueue_style('roots_main', get_template_directory_uri() . '/assets/css/main.min.css', false, '28165fa3d057889b6adcfbaac086fe10');
+  wp_enqueue_style('roots_main', get_template_directory_uri() . '/assets/css/main.min.css', false, '8b475853412507940eed66da5f07de9a');
 
   // jQuery is loaded using the same method from HTML5 Boilerplate:
   // Grab Google CDN's latest jQuery with a protocol relative URL; fallback to local if offline
