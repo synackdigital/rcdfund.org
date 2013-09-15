b51f1b50cd43c44d4fdec2c9f8aba335c92df04c
frebro <frebro@gmail.com>
2013-09-15 11:21:36 +0200

    header responsive tweak

--- app/themes/rcdfund/lib/scripts.php
+++ app/themes/rcdfund/lib/scripts.php
@@ -11,7 +11,7 @@
  * 3. /theme/assets/js/main.min.js (in footer)
  */
 function roots_scripts() {
-  wp_enqueue_style('roots_main', get_template_directory_uri() . '/assets/css/main.min.css', false, '95084e970916587ec4f7ed933ae26348');
+  wp_enqueue_style('roots_main', get_template_directory_uri() . '/assets/css/main.min.css', false, 'd3329becbb4c2374b35eb165f95efe32');
 
   // jQuery is loaded using the same method from HTML5 Boilerplate:
   // Grab Google CDN's latest jQuery with a protocol relative URL; fallback to local if offline
