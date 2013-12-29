404c34da53e8140bf8cb4ab4663340f5274bff5e
frebro <frebro@gmail.com>
2013-12-29 16:43:07 +0100

    fixed navbar

--- app/themes/rcdfund/base.php
+++ app/themes/rcdfund/base.php
@@ -12,12 +12,8 @@
     else :
 
       do_action('get_header');
-      // Use Bootstrap's navbar if enabled in config.php
-      if (current_theme_supports('bootstrap-top-navbar')):
-        get_template_part('templates/header-top-navbar');
-      else:
-        get_template_part('templates/header');
-      endif;
+
+      get_template_part('templates/header');
 
       include roots_template_path();
 
