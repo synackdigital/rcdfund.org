f1be12aa71a7574679a78205da87e31d7e2612fe
frebro <frebro@gmail.com>
2014-01-02 23:54:50 +0100

    color fill utils

--- app/themes/rcdfund/page-wireframe.php
+++ app/themes/rcdfund/page-wireframe.php
@@ -1,7 +1,7 @@
 <?php get_template_part('templates/page', 'header'); ?>
 
 <?php while (have_posts()) : the_post(); ?>
-<section class="section" id="main" role="main">
+<section class="section" id="main" role="main" style="padding-bottom:300px;">
   <div class="container">
     <div class="row">
       <div class="col-lg-12">
@@ -11,3 +11,57 @@
   </div>
 </section>
 <?php endwhile; ?>
+
+<section class="fill-darkblue">
+  <div class="container">
+    darkblue <a href="#">Link</a>
+  </div>
+</section>
+
+<section class="fill-teal">
+  <div class="container">
+    teal <a href="#">Link</a>
+  </div>
+</section>
+
+<section class="fill-sand">
+  <div class="container">
+    sand <a href="#">Link</a>
+  </div>
+</section>
+
+<section class="fill-dust">
+  <div class="container">
+    dust <a href="#">Link</a>
+  </div>
+</section>
+
+<section class="fill-nearwhite">
+  <div class="container">
+    nearwhite <a href="#">Link</a>
+  </div>
+</section>
+
+<section class="fill-yellow">
+  <div class="container">
+    yellow <a href="#">Link</a>
+  </div>
+</section>
+
+<section class="fill-pink">
+  <div class="container">
+    pink <a href="#">Link</a>
+  </div>
+</section>
+
+<section class="fill-red">
+  <div class="container">
+    red <a href="#">Link</a>
+  </div>
+</section>
+
+<section class="fill-brown">
+  <div class="container">
+    brown <a href="#">Link</a>
+  </div>
+</section>
