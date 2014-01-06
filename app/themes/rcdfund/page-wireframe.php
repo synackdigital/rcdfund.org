47d8fbae917a94b9ca366853963b17fddd4cb247
frebro <frebro@gmail.com>
2014-01-06 12:05:09 +0100

    type and buttons styles

--- app/themes/rcdfund/page-wireframe.php
+++ app/themes/rcdfund/page-wireframe.php
@@ -14,25 +14,54 @@
 
 <section class="fill-darkblue">
   <div class="container">
-    darkblue <a href="#">Link</a>
+    <div class="row">
+      <div class="col-lg-6 col-lg-offset-3 text-center">
+        <h3>A dark blue section</h3>
+        <p>This is a text that contains a link.</p>
+        <p><a class="btn btn-success">A button</a></p>
+      </div>
+    </div>
   </div>
 </section>
 
 <section class="fill-teal">
   <div class="container">
-    teal <a href="#">Link</a>
+    <div class="row">
+      <div class="col-lg-12">
+        <h2>A teal section</h2>
+        <p>
+          <div class="btn-toolbar">
+            <div class="btn-group">
+              <button class="btn btn-red">Button 1:1</button>
+              <button class="btn btn-red">Button 1:2</button>
+            </div>
+            <div class="btn-group">
+              <button class="btn btn-red">Button 2:1</button>
+              <button class="btn btn-red">Button 2:2</button>
+              <button class="btn btn-red">Button 2:3</button>
+            </div>
+          </div>
+        </p>
+      </div>
+    </div>
   </div>
 </section>
 
 <section class="fill-sand">
   <div class="container">
-    sand <a href="#">Link</a>
+    <div class="row">
+      <div class="col-lg-12">
+        <h2>A sand colored section</h2>
+        <p><a class="btn btn-darkblue">A button<br>with two rows</a></p>
+      </div>
+    </div>
   </div>
 </section>
 
 <section class="fill-dust">
   <div class="container">
-    dust <a href="#">Link</a>
+    <h3>A dust-colored section</h3>
+    <p class="h3 cursive">A cursive text using Sacramento from Google Web Fonts.</p>
   </div>
 </section>
 
