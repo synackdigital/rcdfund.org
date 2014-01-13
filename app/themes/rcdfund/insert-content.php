bc814e48b40b5975c521c5387f4af44cdbf1edee
frebro <frebro@gmail.com>
2014-01-13 16:13:53 +0100

    dynamic title classes on content

--- app/themes/rcdfund/insert-content.php
+++ app/themes/rcdfund/insert-content.php
@@ -3,6 +3,6 @@ the_post();
 $the_title = ( get_field('title_html') ) ? get_field('title_html') : get_the_title();
 ?>
 <section>
-  <h1 class="h2"><?php echo $the_title; ?></h1>
+  <h1 class="<?php echo get_field('title_class'); ?>"><?php echo $the_title; ?></h1>
   <?php echo the_content(); ?>
 </section>
