b840d4590116cb529f76a72c806afe66fa3acac4
frebro <frebro@gmail.com>
2014-01-14 16:23:49 +0100

    more content containers

--- app/themes/rcdfund/templates/content-brain-matters.php
+++ app/themes/rcdfund/templates/content-brain-matters.php
@@ -1,8 +1,21 @@
 <section class="fill-pink">
   <div class="container">
     <div class="row text-center pt-1 pb-2">
-      <div class="col-xs-12 col-sm-6 col-sm-offset-3 keyline-vertical">
+      <div class="col-xs-12">
         <?php echo do_shortcode("[insert page='the-brain-is-where-the-heart-is' display='insert-content.php']"); ?>
+      </div>
+    </div>
+  </div>
+</section>
+
+<section class="fill-teal">
+  <div class="container">
+    <div class="row text-center pt-1 pb-2">
+      <div class="col-xs-12 col-sm-6 keyline-left">
+        <?php echo do_shortcode("[insert page='on-connor' display='insert-content.php']"); ?>
+      </div>
+      <div class="col-xs-12 col-sm-6">
+        <img class="img-round" src="<?php echo get_thumbnail_url_by_content_slug('on-connor'); ?>" alt="">
       </div>
     </div>
   </div>
