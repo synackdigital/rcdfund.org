bbf577bd5202d26c21b37b8071d7a5424a91bd3c
frebro <frebro@gmail.com>
2014-01-21 11:37:55 +0100

    implemented connorisms in page templates

---
+++ app/themes/rcdfund/templates/content-connorism.php
@@ -0,0 +1,9 @@
+<section class="fill-red">
+  <div class="container">
+    <div class="row text-center pt-1 pb-1">
+      <div class="col-xs-12">
+        <span class="h4 center-block mb-1">Random connorism</span> <span class="cursive h1">&ldquo;<?php the_title(); ?>&rdquo;</span>
+      </div>
+    </div>
+  </div>
+</section>
--- app/themes/rcdfund/templates/content-page.php
+++ app/themes/rcdfund/templates/content-page.php
@@ -17,3 +17,17 @@ if (locate_template('templates/content-' . $post->post_name . '.php') != '') :
   get_template_part('templates/content', $post->post_name);
 endif;
 ?>
+
+<?php
+// Display Connorisms if available
+if ( post_type_exists('connorism') ) :
+  $connorisms = new WP_Query(array(
+    'post_type'       => 'connorism',
+    'orderby'         => 'rand',
+    'posts_per_page'  => 1
+  ));
+  while ( $connorisms->have_posts() ) : $connorisms->the_post();
+    get_template_part('templates/content', 'connorism');
+  endwhile;
+endif;
+?>
