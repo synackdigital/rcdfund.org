b954ed50a0fd2f17ee8137efa834bf848d2d30ad
frebro <frebro@gmail.com>
2014-01-23 06:50:09 +0100

    less prominent connorisms

--- app/themes/rcdfund/templates/content-connorism.php
+++ app/themes/rcdfund/templates/content-connorism.php
@@ -1,8 +1,8 @@
-<section class="fill-red">
+<section class="fill-sand">
   <div class="container">
     <div class="row text-center pt-1 pb-1">
       <div class="col-xs-12">
-        <span class="h4 center-block mb-1">Random connorism</span> <span class="cursive h1">&ldquo;<?php the_title(); ?>&rdquo;</span>
+        <span class="h4 center-block mb-1">Connorism</span> <span class="cursive h2">&ldquo;<?php the_title(); ?>&rdquo;</span>
       </div>
     </div>
   </div>
