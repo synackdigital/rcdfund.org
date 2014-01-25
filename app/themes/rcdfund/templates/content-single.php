84273a742364ab0f31eb69f94f256cc87ed63486
frebro <frebro@gmail.com>
2014-01-25 15:24:19 +0100

    single post title formatting

--- app/themes/rcdfund/templates/content-single.php
+++ app/themes/rcdfund/templates/content-single.php
@@ -1,9 +1,7 @@
 <article class="<?php post_class(); ?>">
   <header class="text-center">
-    <h1 class="h3 entry-title mb-1">
-      <?php echo '<a href="' . get_permalink() . '">' . get_the_title() . '</a>'; ?>
-      <small class="mt-1"><?php get_template_part('templates/entry-meta'); ?></small>
-    </h1>
+    <h1 class="h2 entry-title mb-1"><?php the_title(); ?></h1>
+    <p class="mt-1"><?php get_template_part('templates/entry-meta'); ?></p>
     <img src="/assets/img/squiggle-darkblue.png" class="inline" style="width:340px;height:auto;">
     <figure class="mt-1 mb-2"><?php the_post_thumbnail('large', array( 'class' => 'thumbnail' ) ); ?></figure>
   </header>
