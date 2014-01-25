8c6c1cca8ff4f9b5de933ba453a9b9d448b2b5ea
frebro <frebro@gmail.com>
2014-01-25 13:26:26 +0100

    close #45

--- app/themes/rcdfund/templates/entry-meta.php
+++ app/themes/rcdfund/templates/entry-meta.php
@@ -1,13 +1,12 @@
 <?php if ( get_field('source') ) : ?>
-  &mdash;
   <?php
+  if (get_field('date'))
+    echo date('F j, Y', strtotime(get_field('date'))).', ';
+
   if (get_field('url'))
     echo '<a href="'.get_field('url').'" target="_blank">';
 
   the_field('source');
-
-  if (get_field('date'))
-    echo ', '.get_field('date');
 
   if (get_field('url'))
     echo '</a>';
