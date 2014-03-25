e8b7406edc0c21e91803f37684bbb216366a6f66
frebro <frebro@gmail.com>
2014-03-25 11:19:34 +0100

    tinymce additions

---
+++ web/app/themes/rcdfund/lib/tiny-mce.php
@@ -0,0 +1,26 @@
+<?php
+/**
+ * Tiny MCE additions
+ */
+
+// Activate the Styles dropdown in TinyMCE
+function mce_buttons_2( $buttons ) {
+  array_unshift( $buttons, 'styleselect' );
+  return $buttons;
+}
+add_filter( 'mce_buttons_2', 'mce_buttons_2' );
+
+
+function tiny_mce_before_init( $settings ) {
+  $style_formats = array(
+    array(
+      'title' => 'Lead paragraph',
+      'selector' => 'p',
+      'classes' => 'lead'
+    )
+  );
+  $settings['style_formats'] = json_encode( $style_formats );
+
+  return $settings;
+}
+add_filter( 'tiny_mce_before_init', 'tiny_mce_before_init' );
