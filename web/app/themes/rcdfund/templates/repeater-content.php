f97a672c626162ac0973a2d9d3c81520220e999f
frebro <frebro@gmail.com>
2014-03-25 10:29:14 +0100

    content layout types

--- web/app/themes/rcdfund/templates/repeater-content.php
+++ web/app/themes/rcdfund/templates/repeater-content.php
@@ -1,10 +1,16 @@
 <?php
   // ACF Repeater
   if ( have_rows('content_list') ) : while ( have_rows('content_list') ) : the_row();
-    $post_object          = get_sub_field('post_object');
     $layout               = get_sub_field('layout');
     $background_color     = get_sub_field('background_color');
     $background_texture   = get_sub_field('background_texture');
+
+    $post_object          = get_sub_field('post_object');
+    $post_title           = ( get_field('title_html', $post_object->ID) ) ? get_field('title_html', $post_object->ID) : $post_object->post_title;
+    $post_title_class     = ( get_field('title_class', $post_object->ID) ) ? get_field('title_class', $post_object->ID) : '';
+
+    $post_thumbnail       = ( has_post_thumbnail($post_object->ID) ) ? wp_get_attachment_image_src( get_post_thumbnail_id( $post_object->ID ), 'large' ) : null;
+    $post_thumbnail_url   = ( $post_thumbnail ) ? $post_thumbnail[0] : '';
 ?>
 
 <div class="wrap <?php echo implode(' ', array($layout, $background_color, $background_texture)); ?>">
@@ -12,44 +18,59 @@
   switch ( $layout ) :
 
     // Centered
-    case 'centered': ?>
+    case 'centered' : ?>
       <div class="container">
         <div class="content row">
           <div class="col-xs-12 col-sm-10 col-sm-offset-1 col-md-8 col-md-offset-2 col-lg-6 col-lg-offset-3">
-            centered
+            <article class="type-content text-center">
+              <h1 class="<?php echo $post_title_class; ?>"><?php echo apply_filters( 'the_title', $post_title ); ?></h1>
+              <?php echo apply_filters( 'the_content', $post_object->post_content ); ?>
+            </article>
           </div>
         </div><!-- /.content -->
       </div>
     <?php break;
 
     // Full-width
-    case 'fullwidth': ?>
+    case 'full-width' : ?>
       <div class="content">
-        fullwidth
+        <article class="type-content text-center">
+          <h1 class="<?php echo $post_title_class; ?>"><?php echo apply_filters( 'the_title', $post_title ); ?></h1>
+          <?php echo apply_filters( 'the_content', $post_object->post_content ); ?>
+        </article>
       </div><!-- /.content -->
-    </div><!-- /.wrap -->
     <?php break;
 
     // Thumbnail on left
-    case 'thumbleft': ?>
-      <div class="container">
-        <div class="content row">
-          <div class="col-xs-12 col-sm-8 col-sm-offset-4 col-md-6 col-md-offset-6">
-            right
-          </div>
-        </div><!-- /.content -->
-      </div>
+    case 'featured-image-left' : ?>
+      <div class="featured-image" style="background-image:url('<?php echo $post_thumbnail_url; ?>');">
+        <div class="container">
+          <div class="content row">
+            <div class="col-xs-12 col-sm-5 col-sm-offset-7">
+              <article class="type-content text-center">
+                <h1 class="<?php echo $post_title_class; ?>"><?php echo apply_filters( 'the_title', $post_title ); ?></h1>
+                <?php echo apply_filters( 'the_content', $post_object->post_content ); ?>
+              </article>
+            </div>
+          </div><!-- /.content -->
+        </div>
+      </div><!-- /.featured-image -->
     <?php break;
 
     // Thumbnail on right
-    case 'thumbright': ?>
-      <div class="container">
-        <div class="content row">
-          <div class="col-xs-12 col-sm-8 col-md-6">
-            left
-          </div>
-        </div><!-- /.content -->
-      </div>
+    case 'featured-image-right' : ?>
+      <div class="featured-image" style="background-image:url('<?php echo $post_thumbnail_url; ?>');">
+        <div class="container">
+          <div class="content row">
+            <div class="col-xs-12 col-sm-5">
+              <article class="type-content text-center">
+                <h1 class="<?php echo $post_title_class; ?>"><?php echo apply_filters( 'the_title', $post_title ); ?></h1>
+                <?php echo apply_filters( 'the_content', $post_object->post_content ); ?>
+              </article>
+            </div>
+          </div><!-- /.content -->
+        </div>
+      </div><!-- /.featured-image -->
     <?php break;
 
   endswitch;
