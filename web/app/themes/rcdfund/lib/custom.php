f6bb931f9c9c6db0b2bdc9ac143a34ff051e3ced
frebro <frebro@gmail.com>
2014-03-27 22:37:04 +0100

    in-the-news template and shortcode

--- web/app/themes/rcdfund/lib/custom.php
+++ web/app/themes/rcdfund/lib/custom.php
@@ -2,3 +2,9 @@
 /**
  * Custom functions
  */
+
+// [in-the-news]
+function shortcode_in_the_news() {
+  get_template_part('templates/category', 'in-the-news');
+}
+add_shortcode( 'in-the-news', 'shortcode_in_the_news' );
