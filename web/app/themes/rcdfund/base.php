aac0cbdda4fe6a84abf8e5ccfadf50d0eff494f8
frebro <frebro@gmail.com>
2014-03-28 08:49:11 +0100

    dev cleanup

--- web/app/themes/rcdfund/base.php
+++ web/app/themes/rcdfund/base.php
@@ -1,5 +1,5 @@
 <?php get_template_part('templates/head'); ?>
-<body <?php body_class(); ?> style="min-height:3000px;">
+<body <?php body_class(); ?>>
 
   <!--[if lt IE 8]>
     <div class="alert alert-warning">
