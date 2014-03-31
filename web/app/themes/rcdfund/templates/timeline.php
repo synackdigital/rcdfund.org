10e8bf26e6008578b8754fb5c97cb9c18135365d
frebro <frebro@gmail.com>
2014-03-31 20:00:10 +0200

    timeline template

---
+++ web/app/themes/rcdfund/templates/timeline.php
@@ -0,0 +1,90 @@
+<?php
+// Static template for the timeline feature
+?>
+
+<ol class="list-unstyled mt-sm">
+  <li class="row">
+    <div class="col-sm-2 pb-xs col-sm-offset-5 centerline">
+      <img src="<?php echo IMAGES_DIRECTORY ?>/timeline/connor.jpg" alt="" class="img-circle img-outline">
+    </div>
+    <div class="col-sm-4 text-left">
+      <p class="h2 cursive">April 20, 2013</p>
+      <p>Connor’s 16 month battle with an ependymoma brain tumour ends</p>
+    </div>
+  </li>
+  <li class="row">
+    <div class="col-sm-4 col-sm-offset-1 text-right">
+      <p class="h2 cursive">May, 2013</p>
+      <p>RCD Fund starts with a vision to support brain matters in Research, Care &amp; Development</p>
+    </div>
+    <div class="col-sm-2 pb-xs centerline">
+      <img src="<?php echo IMAGES_DIRECTORY ?>/rcdfund-blue-sbm.png" alt="" class="img-circle img-outline">
+    </div>
+  </li>
+  <li class="row">
+    <div class="col-sm-4 col-sm-offset-1 text-right">
+      <p class="h2 cursive">August, 2013</p>
+      <p>Pippa is the first young brain tumour patient to recieve music and yoga therapy support</p>
+    </div>
+    <div class="col-sm-2 pb-xs centerline">
+      <img src="<?php echo IMAGES_DIRECTORY ?>/timeline/heart.png" alt="" class="img-circle img-outline">
+    </div>
+  </li>
+  <li class="row">
+    <div class="col-sm-2 pb-xs col-sm-offset-5 centerline">
+      <img src="<?php echo IMAGES_DIRECTORY ?>/timeline/map.png" alt="" class="img-circle img-outline">
+    </div>
+    <div class="col-sm-4 text-left">
+      <p class="h2 cursive">September, 2013</p>
+      <p>Connor’s Run is held, with 2&thinsp;000 participants raising over <strong>$100&thinsp;000</strong></p>
+    </div>
+  </li>
+  <li class="row">
+    <div class="col-sm-4 col-sm-offset-1 text-right">
+      <p class="h2 cursive">December, 2013</p>
+      <p>The RCD Fund starts a 3 year <strong>$25&thinsp;000</strong> PhD pediatric brain tumour research scholarship at the Brain Cancer Discovery Collaborative</p>
+    </div>
+    <div class="col-sm-2 pb-xs centerline">
+      <img src="<?php echo IMAGES_DIRECTORY ?>/timeline/ekolv.png" alt="" class="img-circle img-outline">
+    </div>
+  </li>
+  <li class="row">
+    <div class="col-sm-2 pb-xs col-sm-offset-5 centerline">
+      <img src="<?php echo IMAGES_DIRECTORY ?>/timeline/christmas.jpg" alt="" class="img-circle img-outline">
+    </div>
+    <div class="col-sm-4 text-left">
+      <p class="h2 cursive">December 4&ndash;6, 2013</p>
+      <p>The Very Merry Christmas Market spreads Christmas cheer and raises over <strong>$30&thinsp;000</strong></p>
+    </div>
+  </li>
+  <li class="row">
+    <div class="col-sm-4 col-sm-offset-1 text-right">
+      <p class="h2 cursive">December 6, 2013</p>
+      <p>Monash Medical Centre is given <strong>$2&thinsp;500</strong> towards new music therapy equipment to be used with Acquired Brain Injury patients</p>
+    </div>
+    <div class="col-sm-2 pb-xs centerline">
+      <img src="<?php echo IMAGES_DIRECTORY ?>/timeline/note.png" alt="" class="img-circle img-outline">
+    </div>
+  </li>
+  <li class="row">
+    <div class="col-sm-2 pb-xs col-sm-offset-5 centerline">
+      <img src="<?php echo IMAGES_DIRECTORY ?>/timeline/run.png" alt="" class="img-circle img-outline">
+    </div>
+    <div class="col-sm-4 text-left">
+      <p class="h2 cursive">January 26, 2014</p>
+      <p>Connor’s Run receives an Australia Day award for Bayside Community Event of the Year</p>
+    </div>
+  </li>
+  <li class="row">
+    <div class="col-sm-4 col-sm-offset-1 text-right">
+      <p class="h2 cursive">February, 2014</p>
+      <p>The RCD Fund becomes officially incorporated in the USA</p>
+    </div>
+    <div class="col-sm-2 pb-md centerline">
+      <img src="<?php echo IMAGES_DIRECTORY ?>/timeline/usa.png" alt="" class="img-circle img-outline">
+    </div>
+  </li>
+  <li class="row pt-xs">
+    <p class="lead col-sm-6 col-sm-offset-3">With research on the verge of exciting breakthroughs, together we’re making brains matter</p>
+  </li>
+</ol>
