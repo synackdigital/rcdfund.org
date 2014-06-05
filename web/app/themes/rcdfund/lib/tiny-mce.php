c5137d1f3a85de5b02e69503385bc5dc332ce8a8
frebro <frebro@gmail.com>
2014-06-05 22:35:31 +0200

    close #77

--- web/app/themes/rcdfund/lib/tiny-mce.php
+++ web/app/themes/rcdfund/lib/tiny-mce.php
@@ -14,6 +14,51 @@ add_filter( 'mce_buttons_2', 'mce_buttons_2' );
 function tiny_mce_before_init( $settings ) {
   $style_formats = array(
     array(
+      'title' => 'Button, dark blue',
+      'selector' => 'a',
+      'classes' => 'btn btn-darkblue'
+    ),
+    array(
+      'title' => 'Button, teal',
+      'selector' => 'a',
+      'classes' => 'btn btn-teal'
+    ),
+    array(
+      'title' => 'Button, sand',
+      'selector' => 'a',
+      'classes' => 'btn btn-sand'
+    ),
+    array(
+      'title' => 'Button, dust',
+      'selector' => 'a',
+      'classes' => 'btn btn-dust'
+    ),
+    array(
+      'title' => 'Button, near white',
+      'selector' => 'a',
+      'classes' => 'btn btn-nearwhite'
+    ),
+    array(
+      'title' => 'Button, yellow',
+      'selector' => 'a',
+      'classes' => 'btn btn-yellow'
+    ),
+    array(
+      'title' => 'Button, pink',
+      'selector' => 'a',
+      'classes' => 'btn btn-pink'
+    ),
+    array(
+      'title' => 'Button, red',
+      'selector' => 'a',
+      'classes' => 'btn btn-red'
+    ),
+    array(
+      'title' => 'Button, brown',
+      'selector' => 'a',
+      'classes' => 'btn btn-brown'
+    ),
+    array(
       'title' => 'Lead paragraph',
       'selector' => 'p',
       'classes' => 'lead'
