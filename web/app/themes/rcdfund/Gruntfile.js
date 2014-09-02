50d82b5f7201ccdca6e6dd019db714a1941d14e2
frebro <frebro@gmail.com>
2014-09-02 13:47:34 +0200

    close #95 : implemented navbar dropdowns

--- web/app/themes/rcdfund/Gruntfile.js
+++ web/app/themes/rcdfund/Gruntfile.js
@@ -41,7 +41,7 @@ module.exports = function(grunt) {
             // 'bower_components/bootstrap/js/button.js',
             'bower_components/bootstrap/js/carousel.js',
             'bower_components/bootstrap/js/collapse.js',
-            // 'bower_components/bootstrap/js/dropdown.js',
+            'bower_components/bootstrap/js/dropdown.js',
             // 'bower_components/bootstrap/js/modal.js',
             // 'bower_components/bootstrap/js/tooltip.js',
             // 'bower_components/bootstrap/js/popover.js',
