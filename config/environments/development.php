57072be8905a19a8f7ea486006380134585d88ff
frebro <frebro@gmail.com>
2014-09-05 16:02:52 +0200

    extra dev error logging

--- config/environments/development.php
+++ config/environments/development.php
@@ -11,3 +11,7 @@ define('WP_SITEURL', getenv('WP_SITEURL'));
 define('SAVEQUERIES', true);
 define('WP_DEBUG', true);
 define('SCRIPT_DEBUG', true);
+
+ini_set('display_errors','1');
+ini_set('display_startup_errors','1');
+error_reporting (E_ALL);include('index.php');
